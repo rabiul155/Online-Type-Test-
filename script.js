37f33ddec5f4406408695482938fdfe7c9ad884e
rabiul155 <siyamict@gmail.com>
2022-09-15 12:44:41 +0600

    time set into int

--- script.js
+++ script.js
@@ -127,6 +127,7 @@ const start = () => {
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
+
     }
     count--;
   }, 1000);
@@ -141,7 +142,10 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+
+  // parseInt set the time into int formet
+  const timeSpent = parseInt((currentTime - startTime) / 1000);
+
 
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
